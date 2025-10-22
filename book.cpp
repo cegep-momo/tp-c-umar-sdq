e2e1b5d5736b58210ec3f9672119beb45608b2e0
Umar Siddiqui <2377328@cmontmorency.qc.ca>
2025-10-21 23:45:41 -0400

    [FAIT]: methodes pour acceder et modifier (get et set)

--- book.cpp
+++ book.cpp
@@ -11,6 +11,30 @@ Book::Book(const string& title, const string& author, const string& isbn)
 : title(title), author(author), isbn(isbn) {
 
 }
-
+//destrc.
 Book::Book(){}
+
+//get
+
+string Book::getTitle() const {
+    return title;
+}
+
+string Book::getAuthor() const {
+    return author;
+}
+
+string Book::getISBN() const {
+    return isbn;
+}
+//set
+void Book::setTitle(const string& title) {
+    this->title = title;
+}
+void Book::setAuthor(const string& author) {
+    this->author = author;
+}
+void Book::setISBN(const string& isbn) {
+    this->isbn = isbn;
+}
 
